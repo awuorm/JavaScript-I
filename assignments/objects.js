361e2902dc2d8154109071b28ada9ef8c4c8bfb3
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-08-19 14:44:02 +0300

    Finished challenge 1 and 2

--- assignments/objects.js
+++ assignments/objects.js
@@ -19,19 +19,60 @@ const example = {
 
 // Write your intern objects here:
 
+const mitzi = {
+  id: 0,
+  name: "Mitzi",
+  email: "mmelloy0@psu.edu",
+  gender: "F",
+
+}
+
+const kennan = {
+  id: 0,
+  name: "Kennan",
+  email: "kdiben1@tinypic.com",
+  gender: "M",
+}
+
+const keven = {
+  id: 0,
+  name: "Keven",
+  email:"kmummery2@wikimedia.org",
+  gender: "M",
+}
+
+const gannie = {
+  id: 0,
+  name: "Gannie",
+  email: "gmartinson3@illinois.edu",
+  gender: "M",
+}
+
+const antonietta = {
+  id: 0,
+  name: "Antonietta",
+  email: "adaine5@samsung.com",
+  gender: "F",
+}
+
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(mitzi.name);
 
 // Kennan's ID
+console.log(kennan.id);
 
 // Keven's email
+console.log(keven.email);
 
 // Gannie's name
+console.log(gannie.name);
 
 // Antonietta's Gender
+console.log(antonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
