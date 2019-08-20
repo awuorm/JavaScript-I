fd3cf4c2eab6675e3287a5fad9cf1d4d9e8467ac
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-08-20 17:32:27 +0300

    Corrections by Karim

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -80,8 +80,8 @@ console.log(`Car 33 is a ${inventory[32].car_year} ${inventory[32].car_make} ${i
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = 0;
-lastCar = inventory.length-1;
-console.log(`The last car is ${inventory[lastCar].car_make} ${inventory[lastCar].car_model}`);
+lastCar = inventory[inventory.length - 1];
+console.log(`The last car is ${lastCar.car_make} ${lastCar.car_model}`);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
@@ -101,7 +101,7 @@ let carYears = [];
 
 for (let i = 0 ; i < inventory.length ; i++) {
 
-  carYears.push(inventory[i].car_year);
+ carYears =  carYears.push(inventory[i].car_year);
 }
 console.log(carYears);
 
@@ -109,9 +109,9 @@ console.log(carYears);
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars = [];
 
-for (let i = 0 ; i < carYears.length ; i++){
+for (let i = 0; i < carYears.length; i++) {
 
-    if (carYears[i] >2000 ){
+    if (carYears[i] > 2000 ){
 
       oldCars.push(carYears[i])
     }
--- assignments/objects.js
+++ assignments/objects.js
@@ -109,23 +109,28 @@ console.log(antonietta.multiplyNums(3,4));
 const parent = {
   name: "Susan",
   age: 70,
+  speak = () => `My name is ${this.name}`,
 
    child:  {
      name: "George",
      age: 50,
+     
+     speak = () => `My name is ${this.name}`,
 
       grandchild: {
         name: "Sam",
         age: 30,
+        speak = () => `My name is ${this.name}`,
+
       }
     }
 }
 
-speak = ( ) => this.name
+// const speak = ( ) =>  this.name 
 
 // Log the parent object's name
-this.name = parent.name;
-console.log("Hello my name is " + speak());
+// this.name = parent.name;
+console.log(parent.speak());
 
 // Log the child's age
 this.name = parent.child.age;
